aa04f2afe16ee764eae1b1b3032a036d0b106eb6
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-07-20 14:19:36 +0200

    i18n: add missing FR translations

---
+++ i18n_fr.lua
@@ -0,0 +1,4 @@
+return {
+    ["Blacken cells so no number repeats in any row or column."] = { fr = "Noircissez des cases pour qu'aucun chiffre ne se répète sur une ligne ou une colonne." },
+    ["Hitori"] = { fr = "Hitori" },
+}
