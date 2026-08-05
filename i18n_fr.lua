a7c3c31e9347de34de19d1f010619f2527cb69cd
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 14:59:06 +0200

    feat: add ES and DE translations

--- i18n_fr.lua
+++ i18n_fr.lua
@@ -1,4 +1,4 @@
 return {
-    ["Blacken cells so no number repeats in any row or column."] = { fr = "Noircissez des cases pour qu'aucun chiffre ne se répète sur une ligne ou une colonne." },
-    ["Hitori"] = { fr = "Hitori" },
+    ["Blacken cells so no number repeats in any row or column."] = { fr = "Noircissez des cases pour qu'aucun chiffre ne se répète sur une ligne ou une colonne.", es = "Ennegrece casillas para que ningún número se repita en fila o columna.", de = "Schwärze Zellen, sodass sich keine Zahl in einer Zeile oder Spalte wiederholt." },
+    ["Hitori"] = { fr = "Hitori", es = "Hitori", de = "Hitori" },
 }
