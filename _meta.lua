cf70470c1c712a5c37e2693ba5a5d1db213f4e7d
t2ym5u <165804188+t2ym5u@users.noreply.github.com>
2026-08-05 15:05:49 +0200

    chore: bump version to 1.1.15

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ local _ = require("gettext")
 return {
     fullname    = _("Hitori"),
     description = _("Blacken cells so no number repeats in any row or column."),
-    version     = "1.1.14",
+    version     = "1.1.15",
 }
